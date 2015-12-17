d3adadfbb72f3861c27ed436fedbad4768974901
Glauberthy <=>
2015-12-17 15:38:00 -0300

    inicio

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('pricipal').controller('FotosController', function ($scope) {
+    $scope.foto = {
+        titulo: 'Simples teste',
+        url: 'http://www.statusimagens.com/wp-content/uploads/2014/07/deus_dia_vida.jpg'
+    };
+
+    
+});
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+angular.module('pricipal', []);//nome do modulo = pricipal
